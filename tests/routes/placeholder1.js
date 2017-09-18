5040ea4265f5bf546ea034b0a508715a6e867a47
mscharlock <michelle.scharlock@gmail.com>
2017-09-18 13:39:54 -0700

    Trying out the test?

--- tests/routes/placeholder1.js
+++ tests/routes/placeholder1.js
@@ -1,7 +1,10 @@
 'use strict';
+require('jest');
+require('../../lib/server');
+require('superagent');
 
-describe('Test to get Travis to work', () => {
+describe('Test to get Travis to work', function() {
   test('let us see if Travis is working', () =>{
-    expect(true).toBe(true);
+    expect(true).toBeTruthy();
   });
 });
