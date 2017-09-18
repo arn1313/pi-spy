3d46a04de73651e9695a741b6bf587fbd002af65
Zachary Johnson <zaj6827@gmail.com>
2017-09-18 16:11:30 -0700

    added to README, created Storage Schema

--- models/storage.js
+++ models/storage.js
@@ -1,10 +1,18 @@
 'use strict';
 
+
+const debug = require('debug')('piSpy:storage');
+const mongoose = require('mongoose');
+
 // Create Storage Schema? Do we need this or are we using S3 for this?
 // Similar to gallery example?
 // name, id, timestamp.
+const Storage = mongoose.Schema({
+  name : {type : String, required : true},
+  // Make Storage aware of user it belongs to.
+  userId : { type : mongoose.Schema.Types.ObjectId, required : true, ref: 'user'}
+}, {timestamps : true});
 
+//Export
 
-// Make Storage aware of user it belongs to.
-
-//Export this to MongoDB?
+module.exports = mongoose.model('storage', Storage);
