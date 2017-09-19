c73ec0cd7bc8e40bed3a75c4ab1aafaf24949c0e
Shaun Wahle <shaun.wahle@gmail.com>
2017-09-19 14:32:58 -0700

    google-auth

---
+++ routes/get-drive.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+const User = require('../model/user');
+const errorHandler = require('../lib/error-handler');
+const debug = require('debug')('cfgram:route-gallery');
+const basicAuth = require('../lib/bearer-auth')
+const bearerAuth = require('../lib/bearer-auth');
+
+module.exports = function(router) {
+
+  router.get('/api/gallery/:_id', bearerAuth, (req, res) => {
+    debug('GET /api/gallery/:_id');
+
+    return User.findById(req.params._id)
+      .then(gallery => res.json(gallery))
+      .catch(err => errorHandler(err, req, res));
+  });
+};
