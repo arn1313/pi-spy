03d90b4f4e63d16bec8119ff915970fb790c46f1
Shaun Wahle <shaun.wahle@gmail.com>
2017-09-20 10:08:26 -0700

    working on 02auth

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 'use strict';
 
+
+
 require('dotenv').config();
 require('./lib/server').start();
+
+// GET https:'//www.googleapis.com/drive/v2/files/fileId'
+
+
+//
+// fetch('./api/some.json')
+//   .then(
+//     function(response) {
+//       if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
+//
+//       // Examine the text in the response
+//       response.json().then(function(data) {
+//         console.log(data);
+//       });
+//     }
+//   )
+//   .catch(function(err) {
+//     console.log('Fetch Error :-S', err);
+//   });
