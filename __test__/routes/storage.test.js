c7c42898beeddae38b51c3b7d7797a23de5a3511
Zachary Johnson <zaj6827@gmail.com>
2017-09-20 15:31:58 -0700

    added tests to storage.test.js

--- __test__/routes/storage.test.js
+++ __test__/routes/storage.test.js
@@ -29,23 +29,35 @@ describe('Testing Storage Routes', function() {
 
     describe('Valid requests', () => {
       test('should return a status of 200', () => {
-        expect(this.res.status).toBe(200);
+        return superagent.put(`:4444/api/signup/${this.resPost.body._id}`)
+          .send({ name : faker.name.firstName, username : faker.internet.userName, password: 'fakepass', email: faker.internet.email })
+          .then(expect(this.res.status).toBe(200));
       });
 
       describe('invalid request', () => {
-
+        test('should return a status of 400', () => {
+          return superagent.put(`:4444/api/signup/${this.resPost.body.id}`)
+            .send({ name: faker.name.firstName})
+            .then(expect(this.res.status).toBe(400));
+        });
       });
     });
 
     describe('GET', function() {
       describe('valid requests', () => {
-
+        test('should return a 204', () => {
+          return superagent.get(`:4444/api/signup/${this.resPost.body.id}`);
+        });
       });
 
       describe('invalid request', () => {
-
+        test('should return a 400', () => {
+          return superagent.get(':4444/api/signup/fakepath')
+            .then(expect(this.res.status).toBe(400));
+        });
       });
     });
+
     describe('DELETE', function() {
       describe('valid requests', () => {
 
