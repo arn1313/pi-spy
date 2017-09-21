4f78e5bc1b9fabadb5ee10adeba1b601b92e0936
Zachary Johnson <zaj6827@gmail.com>
2017-09-21 11:46:12 -0700

    started running tests, errors everywhere.

--- __test__/routes/user.test.js
+++ __test__/routes/user.test.js
@@ -2,100 +2,60 @@
 
 const faker = require('faker');
 const mocks = require('../lib/mocks');
-// const User = require('../../models/user');
+const User = require('../../models/user');
 const superagent = require('superagent');
 const server = require('../../lib/server');
 require('jest');
 
-describe('Testing basic auth routes', function() {
+describe('Testing User Routes', function() {
   beforeAll(server.start);
   afterAll(server.stop);
-  // afterEach(mocks.user.removeAll);
+  afterEach(mocks.user.removeAll);
 
-  describe('POST to api/signup', function() {
+  describe('POST', function() {
     beforeAll(() => {
-      this.mockUserData = {
-        username: faker.internet.userName(),
-        password: faker.internet.password(),
-        email: faker.internet.email()
-      };
+      this.userFake = { name : faker.name.firstName, username : faker.internet.userName, password: 'fakepass', email: faker.internet.email };
 
-      return superagent.post(':4444/api/signup')
-        .send(this.mockUserData)
-        .then(res => this.res = res)
-        .catch(console.error);
+      return mocks.user.createOne()
+        .then(userData => this.userData = userData)
+        .then(() => {
+          return superagent.post(':4404/api/storage')
+            .set('Authorization', `Bearer ${this.userData.token}`)
+            .send(this.storageFake);
+        })
+        .then(res => this.res = res);
     });
 
-    describe('valid requests', () => {
-      test('make Travis happy', () => {
-        expect(true).toBeTruthy();
+    describe('Valid requests', () => {
+      test('should return a status of 200', () => {
+        return superagent.post(`:4404/api/signup/${this.resPost.body._id}`)
+          .send(this.userFake)
+          .then(expect(this.res.status).toBe(200));
+      });
+
+      xdescribe('invalid request', () => {
+        test('should return a status of 400', () => {
+          return superagent.post(`:4404/api/signup/${this.resPost.body.id}`)
+            .send({ name: faker.name.firstName})
+            .then(expect(this.res.status).toBe(400));
+        });
+      });
+    });
+
+    xdescribe('GET', function() {
+      describe('valid requests', () => {
+        test('should return a 204', () => {
+          return superagent.get(`:4404/api/signup/${this.resPost.body.id}`)
+            .then(expect(this.res.status).toBe(204));
+        });
+      });
+    });
+
+    xdescribe('invalid request', () => {
+      test('should return a 404', () => {
+        return superagent.get(':4404/api/signup/fakepath')
+          .then(expect(this.res.status).toBe(404));
       });
     });
   });
 });
-
-
-//
-// describe('Testing User Routes', function() {
-//   beforeAll(server.start);
-//   afterAll(server.stop);
-//   afterEach(mocks.user.removeAll);
-//   afterEach(mocks.storage.removeAll);
-//
-//   describe('POST', function() {
-//     beforeAll(() => {
-//       this.storageFake = { name: faker.random.word(), desc: faker.random.words(12) };
-//
-//       return mocks.user.createOne()
-//         .then(userData => this.userData = userData)
-//         .then(() => {
-//           return superagent.post(':4444/api/storage')
-//             .set('Authorization', `Bearer ${this.userData.token}`)
-//             .send(this.storageFake);
-//         })
-//         .then(res => this.res = res);
-//     });
-//
-//     describe('Valid requests', () => {
-//       test('should return a status of 200', () => {
-//         return superagent.put(`:4444/api/signup/${this.resPost.body._id}`)
-//           .send({ name : faker.name.firstName, username : faker.internet.userName, password: 'fakepass', email: faker.internet.email })
-//           .then(expect(this.res.status).toBe(200));
-//       });
-//
-//       describe('invalid request', () => {
-//         test('should return a status of 400', () => {
-//           return superagent.put(`:4444/api/signup/${this.resPost.body.id}`)
-//             .send({ name: faker.name.firstName})
-//             .then(expect(this.res.status).toBe(400));
-//         });
-//       });
-//     });
-//
-//     describe('GET', function() {
-//       describe('valid requests', () => {
-//         test('should return a 204', () => {
-//           return superagent.get(`:4444/api/signup/${this.resPost.body.id}`);
-//         });
-//       });
-//
-//       describe('invalid request', () => {
-//         test('should return a 400', () => {
-//           return superagent.get(':4444/api/signup/fakepath')
-//             .then(expect(this.res.status).toBe(400));
-//         });
-//       });
-//     });
-//
-//     describe('DELETE', function() {
-//       describe('valid requests', () => {
-//
-//       });
-//
-//       describe('invalid request', () => {
-//
-//       });
-//     });
-//
-//   });
-// });
