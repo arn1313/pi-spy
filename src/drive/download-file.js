bbb08d7585d80f82bafc5a75bd41f54f9a134527
Shaun Wahle <shaun.wahle@gmail.com>
2017-09-21 12:40:33 -0700

    download looks good

--- src/drive/download-file.js
+++ src/drive/download-file.js
@@ -1,14 +1,14 @@
-var auth = require('../google-auth')
-var fs = require('fs')
-var path = require('path')
+var auth = require('../google-auth');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = function(file, outputFolder) {
-	var outputFile = path.join(outputFolder, file.title)
-	return auth()
-		.then(request => new Promise((resolve, reject) => {
-			request(file.downloadUrl)
-				.pipe(fs.createWriteStream(outputFile))
-				.on('error', reject)
-				.on('close', resolve)
-		}))
-}
+  var outputFile = path.join(outputFolder, file.title);
+  return auth()
+    .then(request => new Promise((resolve, reject) => {
+      request(file.downloadUrl)
+        .pipe(fs.createWriteStream(outputFile))
+        .on('error', reject)
+        .on('close', resolve);
+    }));
+};
--- src/get.js
+++ src/get.js
@@ -1,17 +1,19 @@
-const auth = require('./google-auth')
+'use strict';
 
-const baseUrl = 'https://www.googleapis.com/drive/v2/'
+const auth = require('./google-auth');
+
+const baseUrl = 'https://www.googleapis.com/drive/v3/';
 
 module.exports = function(url, opts/* = {}*/) {
-	if(opts === undefined) opts = {}
-	return auth()
-		.then(request => new Promise((resolve, reject) => {
-			request.get(baseUrl + url, opts, function(err, res) {
-				err ? reject(err) : resolve(res)
-			})
-		}))
-		.then(res => {
-			if(res.statusCode >= 300) throw new Error('Bad status: ' + res.statusCode)
-			return JSON.parse(res.body)
-		})
-}
+  if(opts === undefined) opts = {};
+  return auth()
+    .then(request => new Promise((resolve, reject) => {
+      request.get(baseUrl + url, opts, function(err, res) {
+        err ? reject(err) : resolve(res);
+      });
+    }))
+    .then(res => {
+      if(res.statusCode >= 300) throw new Error('Bad status: ' + res.statusCode);
+      return JSON.parse(res.body);
+    });
+};
